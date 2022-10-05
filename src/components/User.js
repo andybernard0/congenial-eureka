141631a10e06de078c17909cc380540d55ea8f30
andybernard0 <for_react_course@yutep.com>
2022-10-05 20:28:42 +0300

    initial commit

---
+++ src/components/User.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+
+
+function User() {
+
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(res => setUser(res.data))
+            .finally(() => setLoading(false));
+    }, [id]);
+
+
+    return (
+        <div>
+            <h1>User Detail</h1>
+
+            {loading && <div>Loading...</div>}
+
+            {!loading && <pre>{JSON.stringify(user, null, 4)}</pre>}
+
+            <br />
+            <br />
+
+            <Link to={`/users/${parseInt(id) + 1}`}>Next User ({parseInt(id) + 1})</Link>
+        </div>
+    )
+}
+
+export default User
---
+++ src/components/Users.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { NavLink, Switch, Route, useRouteMatch } from 'react-router-dom'
+import axios from 'axios'
+
+import User from './User';
+
+function Users() {
+
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
+
+    const { path, url } = useRouteMatch();
+
+    console.log(path, url);
+
+    useEffect(() => {
+        axios('https://jsonplaceholder.typicode.com/users')
+            .then(res => setUsers(res.data))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+
+            {loading && <div>Loading...</div>}
+
+            <ul>
+                {
+                    users.map((user) => (
+                        <li key={user.id}>
+                            <NavLink to={`${url}/${user.id}`}>{user.name}</NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+
+
+            <Switch>
+                <Route exact path={path}>
+                    <h3>Please Select a User</h3>
+                </Route>
+                <Route path={`${path}/:id`} component={User}>
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default Users
